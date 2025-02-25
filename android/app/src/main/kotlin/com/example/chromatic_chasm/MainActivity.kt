d2298dda9129b37999e01c292c572b21cf206b07
Glootea <rybkinsasha1@gmail.com>
2025-02-25 10:01:36 +0300

    android share

--- android/app/src/main/kotlin/com/example/chromatic_chasm/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/chromatic_chasm/MainActivity.kt
@@ -1,6 +1,35 @@
 package com.glootea.chromaticChasm
 
+import android.content.Intent
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    private val CHANNEL = "com.glootea.chromaticChasm/share"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            CHANNEL
+        ).setMethodCallHandler { call, result ->
+            val args = call.arguments as Map<*, *>
+            when (call.method) {
+                "shareLevel" -> shareLevel(args)
+            }
+        }
+    }
+
+    private fun shareLevel(args: Map<*, *>) {
+        val levelString = args["link"] as String? ?: "Error parsing error to link"
+        val sendIntent: Intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, levelString)
+            type = "text/plain"
+        }
+
+        val shareIntent = Intent.createChooser(sendIntent, null)
+        startActivity(shareIntent)
+    }
 }
